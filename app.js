b1e87a2519628879bfc0dba0f72e66132d10ab1d
techgirli <olorunsolamiracle@gmail.com>
2026-01-29 08:23:39 -0500

    Modified some files.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const productRoutes = require("./backend/routes/productRoutes.js");
+const productRoutes = require("./src/routes/productRoutes.js");
 
 dotenv.config();
 const app = express();
