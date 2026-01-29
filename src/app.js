6757c39e2e63e2065dc202a492014801b77c9bf8
techgirli <olorunsolamiracle@gmail.com>
2026-01-29 09:18:53 -0500

    modified some files related to node modules and package.json

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
 const productRoutes = require("./routes/productRoutes.js");
 
 dotenv.config();
-const app = express();
+const app = express;
 
 app.use(express());
 app.use(express.json());
-app.use(cors())
 
 app.use("/api/products", productRoutes);
 
