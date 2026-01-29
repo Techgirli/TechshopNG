04f4e28973089f1c11fdd335e8babf22a5e7fe45
techgirli <olorunsolamiracle@gmail.com>
2026-01-28 22:36:25 -0500

    modified content of .git/COMMIT_EDITMSG:

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const productRoutes = require("./routes/productRoutes");
+const productRoutes = require("./backend/routes/productRoutes.js");
 
 dotenv.config();
 const app = express();
--- config/models/controllers/routes/productRoutes.js
+++ config/models/controllers/routes/productRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const productController = require("../controllers/productController");
 const { getProducts, addProduct } = require("../controllers/productController");
 
 // Public routes
-router.get("/", getProducts);
+router.get("/", productController.getProducts);
 
 // Admin routes
-router.post("/", addProduct);
+router.post("/", productController.addProduct);
 
 module.exports = router;
