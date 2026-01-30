978831e2deccc952cd98b5ed85a4dc22a2cd1fee
Alex <akinjisealex@gmail.com>
2026-01-30 02:24:51 -0800

    fix express app initalization

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const productRoutes = require("./routes/productRoutes.js");
+
+dotenv.config();
+const app = express();
+
+app.use(express());
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
