da3d332fb63358dbfa158894db333a14400f5ff1
techgirli <olorunsolamiracle@gmail.com>
2026-01-29 20:58:14 -0500

    new files and modification

--- frontend/app/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
-});
-
-export const getProducts = () => API.get("/products");
-export const addProduct = (product) => API.post("/products", product);
